5b6dbc20fccab758f87059fcd590c7bea8d6f02f
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-12-05 19:52:47 +0530

    Understanding SSC and CSC
    Understanding server side components and cleint side components :
    Generally NextJs converts all React components to SSC but we can manually change it to CSC by declaring "use client" on top of the page.

---
+++ app/components/hello.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+function Hello(){
+    console.log("I am client component");
+
+    return(
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    )
+    
+}
+
+export default Hello;
