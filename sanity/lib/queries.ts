604a141ddc2fada159a9cf33213f7a83eb8895b6
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-12-17 21:28:15 +0530

    Author Authentication flow
    Implemented user auth using Oauth github and session

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -15,7 +15,7 @@ defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) 
     image,
   }`);
 
-  export const STARTUP_BY_ID_QUERY =
+export const STARTUP_BY_ID_QUERY =
   defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id, 
   title, 
@@ -36,3 +36,15 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`
       _id, views
   }
 `);
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`
+  *[_type == "author" && id == $id][0]{
+      _id,
+      id,
+      name,
+      username,
+      email,
+      image,
+      bio
+  }
+  `);
