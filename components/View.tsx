16d428bfbe1d754a16d0cc3848abeae57846d453
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-12-17 19:56:24 +0530

    Implemented Sanity write client
    Views are updated dynamically using SSR with help of sanity write client API

--- components/View.tsx
+++ components/View.tsx
@@ -1,6 +1,8 @@
 import { client } from "@/sanity/lib/client"
 import Ping from "./Ping"
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from 'next/server'
 
 const formatNumber = (totalViews: number): string => {
   return `${totalViews} view${totalViews !== 1 ? 's' : ''}`;
@@ -12,7 +14,14 @@ const View = async({id} : {id : string}) => {
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id }); 
 
-    //TODO : update no of views
+    after(
+      async () =>
+        await writeClient
+          .patch(id)
+          .set({ views: totalViews + 1 })
+          .commit(),
+    );
+  
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
