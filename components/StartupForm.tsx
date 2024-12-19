86ee7a44ad7b48ef1a776da185ac79a5b21f9e8f
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-12-19 22:04:21 +0530

    Integrate MDEditor for pitch input in startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -2,9 +2,15 @@
 import React, { useState } from 'react'
 import { Input } from './ui/input';
 import { Textarea } from './ui/textarea';
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({})
+  const [pitch, setPitch] = useState("");
+  const isPending = false;
+
   return (
     <form action={() => {}} className='startup-form'>
         <div>
@@ -58,6 +64,33 @@ const StartupForm = () => {
 
           {errors.link && <p className='startup-form_error'>{errors.link}</p>}
         </div>
+
+        <div data-color-mode="light">
+          <label htmlFor='pitch' className='startup-form_label'>Pitch</label>
+          <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder:
+              "Briefly describe your idea and what problem it solves",
+          }}
+          previewOptions={{
+            disallowedElements: ["style"],
+          }}
+          />
+
+          {errors.pitch && <p className='startup-form_error'>{errors.pitch}</p>}
+        </div>
+
+        <Button type='submit' className='startup-form_btn text-white' disabled={isPending}>
+          {isPending ? 'Submitting...' : 'Submit'}
+          <Send className="size-6 ml-2"/>
+        </Button>
+        
     </form>
   )
 }
